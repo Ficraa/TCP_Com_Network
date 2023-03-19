6b6ed8845da0a2586c26d253f1bb79a6d90def84
StyrO <karam.bd.kb@gmail.com>
2023-03-19 17:44:08 +0000

    add functions

--- Server/TCP_SERVER.py
+++ Server/TCP_SERVER.py
@@ -1,4 +1,6 @@
 import socket
+import pyautogui
+import pydirectinput
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(("0.0.0.0", 9090))
@@ -11,7 +13,21 @@ while (True):
     while True:
         try:
             dataFromClient = clientConnected.recv(1024)
-            print(dataFromClient.decode())
+            msg = dataFromClient.decode()
+            print(msg)
+            if msg.rsplit('(', 1)[0] == "move":
+                pydirectinput.moveTo(int(msg[5:].rsplit(',', 1)[0]), int(
+                    msg[5:-1].rsplit(',', 1)[1]))
+            elif msg == "left":
+                pydirectinput.press("left")
+            elif msg == "right":
+                pydirectinput.press("right")
+            elif msg == "up":
+                pydirectinput.press("up")
+            elif msg == "down":
+                pydirectinput.press("down")
+            elif msg == "click":
+                pydirectinput.click()
             clientConnected.send("h".encode())
         except:
             break
